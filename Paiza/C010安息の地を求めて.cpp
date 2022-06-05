4fc0f0dd1c0c83af5dd5d453c805979d76836603
matsumotominoru <matsumoto.minoru.su@alumni.tsukuba.ac.jp>
2022-06-05 12:11:04 +0900

    Modify style of getting input

--- Paiza/C010安息の地を求めて.cpp
+++ Paiza/C010安息の地を求めて.cpp
@@ -1,17 +1,20 @@
 ﻿#include <stdio.h>
 int main(void){
-    int a;
-    int b;
-    int R;
-    int N;
-    int x;
-    int y;
-    
-    scanf_s("%d%d%d",&a,&b,&R);
-    scanf_s("%d",&N);
+    int a, b, R;//工事現場のx座標,工事現場のy座標,工事現場の騒音の大きさ
+    int N;//木陰の数
+    int x, y;//木陰のx座標,木陰のy座標
+    char buf[1000];
+
+    fgets(buf, sizeof(buf), stdin);
+    sscanf(buf, "%d %d %d\n", &a, &b, &R);
+
+    fgets(buf, sizeof(buf), stdin);
+    sscanf(buf, "%d\n", &N);
     
     for(int i =0;i<N;i++){
-        scanf_s("%d%d",&x,&y);
+        fgets(buf, sizeof(buf), stdin);
+        sscanf(buf, "%d %d\n", &x, &y);
+        
         if((x-a)*(x-a)+(y-b)*(y-b) < R*R){
             printf("noisy\n");
         }
